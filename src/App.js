9f7cbddbfe06969089ccf3e4907124d55029c20a
helderscrolls <nortehelder@gmail.com>
2019-05-29 16:06:13 +0200

    added state to change atome logo speed depending on the workingstatus of Homer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 import Quote from "./Quote";
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: false
+    };
+    this.state = {
+      change: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+    // this.setState({});
+  };
+
   render() {
+    const working = this.state.working ? 'Homer Travaille' : 'Homer est en pause';
+    const rest = this.state.working? 'App-logo' : 'App-anim';
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={rest} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Quote 
+
+        <Lamp on />
+        <Lamp />
+
+        <button onClick={this.handleClick} className={working}> {working} </button>
+
+        <Quote
           quote="I believe the children are the future... Unless we stop them now!"
           character="Homer Simpson"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
@@ -22,6 +48,7 @@ class App extends Component {
           character="Ralph Wiggum"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
         />
+        <Quotes />
       </div>
     );
   }
